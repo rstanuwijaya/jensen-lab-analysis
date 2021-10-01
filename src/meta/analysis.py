e6d7f8b73172ebaca9efc9afc75b6d5dd8d6468d
rstanuwijaya <rstanuwijaya@connect.ust.hk>
2021-10-01 14:10:56 +0800

    Added gaussian slots

--- src/meta/analysis.py
+++ src/meta/analysis.py
@@ -1,17 +1,63 @@
 #%%
 import os
 import cv2
+from lmfit.lineshapes import gaussian
+from lmfit.minimizer import minimize
 import numpy as np
 import matplotlib.pyplot as plt
 from random import randint
 from scipy.optimize import curve_fit    
-
+import lmfit
+import math
+from math import sqrt, pi, exp
 class SlotModel:
     CANVAS_X = 3000
     CANVAS_Y = 2000
 
     @staticmethod
+    def gaussian_2d(y, x, mu_y, mu_x, s_y, s_x):
+        return 1. / np.sqrt(2*np.pi*s_x*s_y) * np.exp(-((x-mu_x)**2/(2*s_x**2)+(y-mu_y)**2/(2*s_y**2)))
+
+    @staticmethod
+    def gaussian_slot(y, x, cy, cx, l, w, r):
+        img = np.zeros(x.shape)
+        bl = cx - w/2
+        br = cx + w/2
+        bt = cy - l/2
+        bb = cy + l/2
+        img += SlotModel.gaussian_2d(y, x, bl, bt, r, r)
+        img += SlotModel.gaussian_2d(y, x, br, bt, r, r)
+        img += SlotModel.gaussian_2d(y, x, bl, bb, r, r)
+        img += SlotModel.gaussian_2d(y, x, br, bb, r, r)
+        bt, bb = int(bt), int(bb)
+        bl, br = int(bl), int(br)
+        img[bt:bb, :] = img[:, None][bt, :]
+        img[:, bl:br] = img[:, bl][:, None]
+        return img
+
+    @staticmethod
+    def gaussian_cell(y, x, my, mx, py, px, dx, dy, l, w, r):
+        print(mx, my)
+        img = np.zeros(x.shape)
+        cy1, cx1 = my - dy, mx - dx
+        cy2, cx2 = my + dy, mx + dx
+        img += SlotModel.gaussian_slot(y, x, cy1, cx1, l, w, r)
+        img += SlotModel.gaussian_slot(y, x, cy2, cx2, l, w, r)
+        return img
+    
+    @staticmethod
+    def gaussian_lattice(y, x, y0, x0, py, px, dy, dx, l, w, r):
+        img = np.zeros(x.shape)
+        reps_y = y.shape[0] // py + 1
+        reps_x = x.shape[1] // px + 1
+        for j in range(reps_y):
+            for i in range(reps_x):
+                img += SlotModel.gaussian_cell(y, x, j*py - y0, i*px - x0, py, px, dx, dy, l, w, r)
+        return img
+
+    @staticmethod
     def single_slot(cell, y, x, l, w, r):
+        # rounded rectangular slot oriented vertically
         bl = x - w//2
         br = x + w//2
         bt = y - l//2
@@ -26,6 +72,7 @@ class SlotModel:
 
     @staticmethod
     def slot_cell(py, px, dx, dy, l, w, r):
+        # unit cell containing a pair of single slots located
         py, px = int(py), int(px)
         img = np.zeros((py, px), np.uint8)
         img = SlotModel.single_slot(img, py//2-dy, px//2+dx, l, w, r)
@@ -33,14 +80,17 @@ class SlotModel:
         return img
 
     @staticmethod
-    def slot_lattice(_, y0, x0, py, px, dy, dx, l, w, r):
+    def slot_lattice(xdata, y0, x0, py, px, dy, dx, l, w, r):
+        # lattice of the slots on a given canvas
+        print(y0, x0, py, px, dy, dx, l, w, r,)
         y0, x0, py, px, dy, dx, l, w, r, = int(y0), int(x0), int(py), int(px), int(dy), int(dx), int(l), int(w), int(r),
         canvas_y, canvas_x = SlotModel.CANVAS_Y, SlotModel.CANVAS_X
-        assert canvas_y%py == 0 and canvas_x%px == 0, "Slot cell dimension is not an integer"
+        # assert canvas_y%py == 0 and canvas_x%px == 0, "Slot cell dimension is not an integer"
         reps_y, reps_x = canvas_y//py + 1, canvas_x//px + 1
         cell = SlotModel.slot_cell(py, px, dy, dx, l, w, r)
         img = np.tile(cell, (reps_y, reps_x))
         img =  img[y0:y0+canvas_y, x0:x0+canvas_x]
+        img = cv2.GaussianBlur(img, (101, 101), 0)
         return img.flatten()
 
 class MetaAnalyzer:
@@ -51,7 +101,17 @@ class MetaAnalyzer:
         self.__rectangles = self.draw_rectangles()
         # self.test_slot_cell()
         # self.test_slot_array()
-        self.fit_lattice()
+        # self.fit_lattice()
+        # self.fit_lattice_lmfit()
+        # self.fit_lattice_leastsq()
+
+        # self.test_gaussian_2d()
+        # self.test_gaussian_slot()
+        # self.test_gaussian_cell()
+        # self.test_gaussian_lattice()
+
+        self.fit_ellipse()
+        print("done")
 
     @staticmethod
     def load_image(path):
@@ -75,8 +135,44 @@ class MetaAnalyzer:
             cv2.drawContours(res_img, [c], -1, 255, -1)
         plt.imshow(res_img)
         plt.show()
-        return contours, res_img      
-    
+        return contours, res_img 
+
+    def test_gaussian_2d(self):
+        print("testing gaussian")
+        y = np.linspace(-100, 100, 200)
+        x = np.linspace(-100, 100, 200)
+        y, x = np.meshgrid(y, x)
+        img = SlotModel.gaussian_2d(y, x, 50, 50, 20, 20)
+        plt.imshow(img)
+        plt.show()     
+
+    def test_gaussian_slot(self):
+        print("testing slot")
+        y = np.linspace(0, 500, 400)
+        x = np.linspace(0, 500, 400)
+        y, x = np.meshgrid(y, x)
+        img = SlotModel.gaussian_slot(y, x, 250, 250, 100, 20, 5)
+        plt.imshow(img)
+        plt.show()     
+
+    def test_gaussian_cell(self):
+        print("testing gaussian")
+        y = np.linspace(0, 500, 400)
+        x = np.linspace(0, 500, 400)
+        y, x = np.meshgrid(y, x)
+        img = SlotModel.gaussian_cell(y, x, 250, 250, 200, 200, 50, 50, 100, 20, 5)
+        plt.imshow(img)
+        plt.show()    
+
+    def test_gaussian_lattice(self):
+        print("testing gaussian")
+        y = np.linspace(0, 3000, 200)
+        x = np.linspace(0, 3000, 200)
+        y, x = np.meshgrid(y, x)
+        img = SlotModel.gaussian_lattice(y, x, 0, 0, 500, 500, 250, 250, 100, 20, 5)
+        plt.imshow(img)
+        plt.show()     
+
     def test_slot_cell(self):
         img = SlotModel(self.__image).slot_cell(500, 500, 125, 125, 200, 100, 50)
         plt.imshow(img)
@@ -84,9 +180,7 @@ class MetaAnalyzer:
 
     def test_slot_array(self):
         img_gray = cv2.cvtColor(self.__image, cv2.COLOR_BGR2GRAY)
-        img = SlotModel(self.__image).slot_lattice(img_gray, 200, 100, 500, 500, 125, 125, 200, 100, 50)
-        plt.imshow(img)
-        plt.show()
+        img = SlotModel.slot_lattice(img_gray, 200, 100, 500, 500, 125, 125, 200, 100, 50)
 
     def fit_lattice(self):
         img_gray = cv2.cvtColor(self.__image, cv2.COLOR_BGR2GRAY)
@@ -105,7 +199,67 @@ class MetaAnalyzer:
         print(popt)
         print("pcov:")
         print(pcov)
+
+    def fit_lattice_lmfit(self):
+        img_gray = cv2.cvtColor(self.__image, cv2.COLOR_BGR2GRAY)
+        img_gray = cv2.GaussianBlur(img_gray, (101, 101), 0)
+        self.ydata = img_gray.flatten()
+        self.model = lmfit.Model(SlotModel.slot_lattice, name="test model")
+        default_hint = {'value': 1, 'min': -math.inf, 'max': math.inf, 'vary': False, 'expr': None,}
+        self.init_params = {
+            'y0': {
+                'value': 50,
+                'vary': False,
+            },
+            'x0': {
+                'value': 200,
+                'vary': False,
+            },
+            'py': {
+                'value': 500,
+                'vary': True,
+            },
+            'px': {
+                'value': 500,
+                'vary': True,
+            },
+            'dy': {
+                'value': 500//4,
+                'vary': False,
+            },
+            'dx': {
+                'value': 500//4,
+                'vary': False,
+            },
+            'l': {
+                'value': 200,
+                'vary': True,
+            },
+            'w': {
+                'value': 50,
+                'vary': True,
+            },
+            'r': {
+                'value': 10,
+                'vary': True,
+            },
+        }
+        for param_name in self.model.param_names:
+            self.model.set_param_hint(param_name, 
+                value=self.init_params.get(param_name, default_hint).get('value', default_hint['value']),
+                min=self.init_params.get(param_name, default_hint).get('min', default_hint['min']),
+                max=self.init_params.get(param_name, default_hint).get('max', default_hint['max']),
+                vary=self.init_params.get(param_name, default_hint).get('vary', default_hint['vary']),
+                )
+        self.params = self.model.make_params()
+        self.result = self.model.fit(self.ydata, self.params, xdata=None)
+        print(self.result.fit_report())
         
+    
+    def fit_lattice_leastsq():
+        img_gray = cv2.cvtColor(self.__image, cv2.COLOR_BGR2GRAY)
+        img_gray = cv2.GaussianBlur(img_gray, (101, 101), 0)
+
     def fit_ellipse(self):
         img = self.__image
         contours = self.__contours
@@ -146,7 +300,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# %%
 
 # %%
